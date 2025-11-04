271ad3c8214a8a7a9e5d2d634e09de3e3ba90667
Nawaf Alfaqih <nawafalfaqih@outlook.com>
2025-11-04 14:57:30 +0300

    Commiting answer

--- src/main/java/org/example/challengeweek4day3/Controller.java
+++ src/main/java/org/example/challengeweek4day3/Controller.java
@@ -1,9 +1,92 @@
 package org.example.challengeweek4day3;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.*;
+import java.util.ArrayList;
 
 @RestController
 public class Controller {
+
+    ArrayList<LocalDate> loginData = new ArrayList<>();
+    ArrayList<LocalDate> logoutData = new ArrayList<>();
+    ArrayList<LocalTime> loginTime = new ArrayList<>();
+    ArrayList<LocalTime> logoutTime = new ArrayList<>();
+    ArrayList<String> users = new ArrayList<>();
+    int counter = 0;
+
+    @GetMapping("/loginDate")
+    public LocalDate loginDate() {
+
+        LocalDate date = LocalDate.now();
+
+        loginData.add(date);
+        return date;
+    }
+
+    @GetMapping("/logoutDate")
+    public LocalDate logoutDate() {
+        LocalDate date = LocalDate.now();
+
+        logoutData.add(date);
+
+        return date;
+    }
+
+    @GetMapping("/loginDate/Data")
+    public ArrayList<LocalDate> viewLoginDate() {
+        return loginData;
+    }
+
+    @GetMapping("/logoutDate/Data")
+    public ArrayList<LocalDate> viewLogoutDate() {
+        return logoutData;
+    }
+
+    @GetMapping("/loginTime")
+    public LocalTime loginTime() {
+
+        LocalTime time = LocalTime.now();
+
+        loginTime.add(time);
+        return time;
+    }
+
+    @GetMapping("/logoutTime")
+    public LocalTime logoutTime() {
+        LocalTime time = LocalTime.now();
+
+        logoutTime.add(time);
+
+        return time;
+    }
+
+    @GetMapping("/loginTime/Data")
+    public ArrayList<LocalTime> viewLoginTime() {
+        return loginTime;
+    }
+
+    @GetMapping("/logoutTime/Data")
+    public ArrayList<LocalTime> viewLogoutTime() {
+        return logoutTime;
+    }
+
+    @GetMapping("/Users/Data")
+    public ArrayList<String> viewUsers() {
+        return users;
+    }
+
+    @GetMapping("/logoutTime")
+    public String addUser() {
+        String user = "User "+(++counter);
+
+        users.add(user);
+
+        return user;
+    }
+
+
 
 
 }
